7eace14514db3263b31389c0e7c7547fba173cb7
James Davies <60436624+jimdavies72@users.noreply.github.com>
2021-12-21 17:20:51 +0000

    lts version.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,34 +1,27 @@
 const MovieCard = (props) => {
   return (
     <>
-      <div
-        style={{
-          backgroundImage: `url(${props.backdropSrc})`,
-        }}
-        className="card-container"
-      >
-        <h2 id="card-heading">{props.movieTitle}</h2>
+      <div className="card-container">
+        <h2 id="card-heading">{props.movieTitle} </h2>
 
         <div className="movie-detail">
-          <h3>Movie Rank: {props.key}</h3>
           <div className="overview">
             <img id="poster" src={props.posterSrc} alt={props.movieTitle} />
             <h4>{props.overview}</h4>
           </div>
-
-          <p>Adult film? - {props.adult}</p>
-          <p>Language: {props.language}</p>
-
-          <h4>{props.popularity}</h4>
-          <h4>
-            Score: {props.voteScore}/10 ({props.voteCount} votes counted)
-          </h4>
+          <div className="film-data">
+            <h4>Rank: {props.key}</h4>
+            <h4>Adult film? - {props.adult}</h4>
+            <h4>Language: {props.language}</h4>
+            <h4>Popularity: {props.popularity}</h4>
+            <h4>
+              Score: {props.voteScore}/10 ({props.voteCount} votes counted)
+            </h4>
+          </div>
         </div>
       </div>
     </>
   );
 };
-
-//<img className="backdrop" src={props.backdropSrc} alt={props.movieTitle} />;
 
 export default MovieCard;
